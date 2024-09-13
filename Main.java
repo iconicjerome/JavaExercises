44aafec834623def02f5c174bfe82f7308292205
iconicjerome <iconicjerome@gmail.com>
2024-09-13 13:26:09 +0100

    Add exercise 9

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 
 public class Main{
+  //Methods for exercise 8
   final static double circleArea(double circleRadius){
     double area = Math.PI *Math.pow(circleRadius, 2);
     System.out.println("The area of the circle is "+ area);
@@ -12,6 +13,23 @@ public class Main{
     double perimeter = 2 * Math.PI * circleRadius;
     System.out.println("The perimeter of the circle is "+ perimeter );
     return perimeter;
+  }
+
+  //Method for exercise 9
+  final static int getAverage(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter first number: ");
+    int userNum1 = scanner.nextInt();
+    System.out.println("Enter second number: ");
+    int userNum2 = scanner.nextInt();
+    System.out.println("Enter third number: ");
+    int userNum3 = scanner.nextInt();
+    scanner.close();
+    int addNum = userNum1 + userNum2 + userNum3;
+    int average = addNum / 3;
+    System.out.println("The average is "+ average);
+    return average;
+    
   }
   public static void main(String[]args){
     //URL for these exercises: https://www.w3resource.com/java-exercises/basic/index.php#editorr
@@ -75,14 +93,15 @@ public class Main{
     // scanner.close();
 
     //Exercise 8
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter Radius: ");
-    double radius = scanner.nextDouble();
-    circleArea(radius);
-    circlePerimeter(radius);
-    scanner.close();
+    // Scanner scanner = new Scanner(System.in);
+    // System.out.println("Enter Radius: ");
+    // double radius = scanner.nextDouble();
+    // circleArea(radius);
+    // circlePerimeter(radius);
+    // scanner.close();
+
+    //Exercise 9
+    getAverage();
   }
-
-
   
 }
